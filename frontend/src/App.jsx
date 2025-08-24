715d4c78fd284aa52ad621f7120dba779071c50e
ishantsingh <singhishant37@gmail.com>
2025-08-24 23:28:26 +0530

    Added the background

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,3 @@
-
 import React, { useState } from 'react';
 import Navbar from './components/Navbar';
 import { Route, Routes } from 'react-router-dom';
@@ -8,20 +7,51 @@ import PlaceOrder from './Pages/PlaceOrder/PlaceOrder';
 import Kitchen from './components/Kitchen-items/Kitchen';
 import './App.css';
 import LoginPopup from './components/LoginPagePopUp/LoginPopup';
+import LightRays from './components/React_bits/LightRays';
 
 const App = () => {
   const [showLogin, setshowLogin] = useState(false);
   const [userImg, setUserImg] = useState(null);
+
   return (
     <>
-      {showLogin ? <LoginPopup setshowLogin={setshowLogin} setUserImg={setUserImg} /> : null}
-      <div className='app'>
+      {showLogin && (
+        <LoginPopup setshowLogin={setshowLogin} setUserImg={setUserImg} />
+      )}
+
+      {/* Background LightRays - Always behind */}
+      <div
+        style={{
+          position: 'fixed',
+          top: 0,
+          left: 0,
+          width: '100%',
+          height: '100%',
+          zIndex: -1,
+          overflow: 'hidden'
+        }}
+      >
+        <LightRays
+          raysOrigin="top-center"
+          raysColor="red"
+          raysSpeed={1.5}
+          lightSpread={0.8}
+          rayLength={1.2}
+          followMouse={true}
+          mouseInfluence={0.1}
+          noiseAmount={0.1}
+          distortion={0.05}
+        />
+      </div>
+
+      {/* Foreground Content */}
+      <div className="app" style={{ position: 'relative', zIndex: 1 }}>
         <Navbar setshowLogin={setshowLogin} userImg={userImg} />
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/cart' element={<Cart />} />
-          <Route path='/order' element={<PlaceOrder />} />
-          <Route path='/home' element={<Kitchen />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/order" element={<PlaceOrder />} />
+          <Route path="/home" element={<Kitchen />} />
         </Routes>
       </div>
     </>
